8e7a77a77c481fe093f38b681b03a461c08f3d41
louisua <louisuarasoanandrasana@gmail.com>
2017-10-18 11:28:23 +0300

    test commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+// routing
+import { appRoutes } from './app.routing';
 
+
+// components
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { YoutubePlayerComponent } from './youtube-player/youtube-player.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    TodoListComponent,
+
+    YoutubePlayerComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,28 @@
+import { Routes } from '@angular/router';
+
+// components
+import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { YoutubePlayerComponent } from './youtube-player/youtube-player.component';
+
+// Definition of routes
+export const appRoutes: Routes = [
+  {
+    path : 'todolist',
+    component: TodoListComponent  // TODO : use appropriated page
+  },
+
+  {
+    path : 'player',
+    component: YoutubePlayerComponent // TODO : use appropriated page
+  },
+  {
+    path : 'calculator',
+    component: AppComponent // TODO : use appropriated page
+  },
+  {
+    path     : '**',
+    redirectTo: '',
+  }
+
+];
