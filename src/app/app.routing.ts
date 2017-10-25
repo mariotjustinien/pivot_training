53a5e3d770f61316458cb2bb8f1d617fa621cfdf
mariotjustinien <mariot.se@pivotworks.org>
2017-10-25 15:52:05 +0300

    Commit on app.routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,7 +5,7 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { YoutubePlayerComponent } from './youtube-player/youtube-player.component';
-import { CalculatorComponent } from './calculator/calculator.component'; 
+import { CalculatorComponent } from './calculator/calculator.component';
 
 // Definition of routes
 export const appRoutes: Routes = [
@@ -13,7 +13,6 @@ export const appRoutes: Routes = [
     path : 'todolist',
     component: TodoListComponent  // TODO : use appropriated page
   },
-
   {
     path : 'player',
     component: YoutubePlayerComponent // TODO : use appropriated page
@@ -21,7 +20,6 @@ export const appRoutes: Routes = [
   {
     path : 'calculator',
     component: CalculatorComponent // TODO : use appropriated page
-
   },
   {
     path     : '**',
